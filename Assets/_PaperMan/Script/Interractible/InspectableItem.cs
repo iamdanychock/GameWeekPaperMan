1ff099846ed13f23fec311763345fc3bc06140d1
ValentinPOQUET <valentin.poquet91@gmail.com>
2024-09-09 21:10:00 +0200

    Added Interactable class

---
+++ Assets/_PaperMan/Script/Interractible/InspectableItem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.IsartDigital.PaperMan
+{
+    public class InspectableItem : Interactable
+    {
+        protected override void Interact()
+        {
+            //if (Object3DContainer.Instance)
+            //    Object3DContainer.Instance.Show3DObject();
+        }
+    }
+}
