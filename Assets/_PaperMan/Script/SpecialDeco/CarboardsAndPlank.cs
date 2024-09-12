a49a4f7534dffd65bdbf3e13edd29a1548f99902
ValentinPOQUET <valentin.poquet91@gmail.com>
2024-09-12 12:03:57 +0200

    Added dust on player

--- Assets/_PaperMan/Script/SpecialDeco/CarboardsAndPlank.cs
+++ Assets/_PaperMan/Script/SpecialDeco/CarboardsAndPlank.cs
@@ -6,10 +6,9 @@ public class CarboardsAndPlank : MonoBehaviour
 {
     [SerializeField] private float screenshakeDuration = 1f;
     [SerializeField] private float screenshakeForce = .5f;
-    [SerializeField] private Com.IsartDigital.PaperMan.Camera mainCamera;
 
     public void PlayScreenshake()
     {
-        mainCamera.Shake(screenshakeDuration, screenshakeForce);
+        Com.IsartDigital.PaperMan.Camera.Instance.Shake(screenshakeDuration, screenshakeForce);
     }
 }
